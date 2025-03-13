4851c3df40f8eaab4b25353150a6e2c64773e5eb
Ewan Gravé <grave.ewan2001@gmail.com>
2025-03-13 14:41:17 +0100

    feat: header comp

--- taskhandler-front/src/app/pages/workspacespage/workspacespage.component.ts
+++ taskhandler-front/src/app/pages/workspacespage/workspacespage.component.ts
@@ -1,20 +1,29 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
+import { WorkspaceControllerService, WorkspaceDTO } from '../../../../api';
+import { HeaderComponent } from '../../components/header/header/header.component';
 
 @Component({
   selector: 'app-workspacespage',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './workspacespage.component.html',
   styleUrl: './workspacespage.component.css',
 })
 export class WorkspacespageComponent {
   loginService = inject(LoginService);
+  workspacesService = inject(WorkspaceControllerService);
+  workspaces: WorkspaceDTO[] = [];
   router = inject(Router);
 
   ngOnInit(): void {
-    if (!this.loginService.getLoggedUser()) {
+    const user = this.loginService.getLoggedUser();
+    if (!user || !user.idUser) {
       this.router.navigate(['/login']);
+    } else {
+      this.workspacesService
+        .getWorkspaceByUser(user.idUser)
+        .subscribe((workspaces) => (this.workspaces = workspaces));
     }
   }
 }
